29e7ac28689d5d2e346390557c6da500114638b3
meenal0205 <95573151+meenal0205@users.noreply.github.com>
2023-04-24 18:17:26 +0530

    Initial commit

--- app.py
+++ app.py
@@ -212,68 +212,7 @@ def submit_data1():
     
        
 
-# @app.route('/skiils_job',methods=['GET', 'POST'])
-# def submit_data2():
-#     if request.method == 'POST':
-        
-#         print(request.form['list_jobs'])
-       
-#         resume=list(request.form['list_jobs'])
-#         print(type(resume))
-#         skills=[]
-#         skills.append(' '.join(word for word in resume))
-#         org_name_clean = skills
-        
-#         def ngrams(string, n=3):
-#             string = fix_text(string) 
-#             string = string.encode("ascii", errors="ignore").decode() 
-#             string = string.lower()
-#             chars_to_remove = [")","(",".","|","[","]","{","}","'"]
-#             rx = '[' + re.escape(''.join(chars_to_remove)) + ']'
-#             string = re.sub(rx, '', string)
-#             string = string.replace('&', 'and')
-#             string = string.replace(',', ' ')
-#             string = string.replace('-', ' ')
-#             string = string.title() 
-#             string = re.sub(' +',' ',string).strip()
-#             string = ' '+ string +' ' # pad names for ngrams...
-#             string = re.sub(r'[,-./]|\sBD',r'', string)
-#             ngrams = zip(*[string[i:] for i in range(n)])
-#             return [''.join(ngram) for ngram in ngrams]
-#         vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams, lowercase=False)
-#         tfidf = vectorizer.fit_transform(org_name_clean)
-      
-        
-        
-#         def getNearestN(query):
-#           queryTFIDF_ = vectorizer.transform(query)
-#           distances, indices = nbrs.kneighbors(queryTFIDF_)
-#           return distances, indices
-#         nbrs = NearestNeighbors(n_neighbors=1, n_jobs=-1).fit(tfidf)
-#         unique_org = (df['test'].values)
-#         distances, indices = getNearestN(unique_org)
-#         unique_org = list(unique_org)
-#         matches = []
-#         for i,j in enumerate(indices):
-#             dist=round(distances[i][0],2)
-  
-#             temp = [dist]
-#             matches.append(temp)
-#         matches = pd.DataFrame(matches, columns=['Match confidence'])
-#         df['match']=matches['Match confidence']
-#         df1=df.sort_values('match')
-#         df2=df1[['Position', 'Company','Location']].head(10).reset_index()
-        
-        
-        
-        
-        
-     
-#         return render_template('new_model.html',tables=[df2.to_html(classes='job')],titles=['na','Job'])
-#     return render_template("new_model.html")
-    
-
      
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8080)
